77826cd39cc62ebd8ee6da4fc7816fbbebde4d05
filipedantas800 <filipetecnico800@hotmail.com>
2023-07-04 12:38:27 -0300

    ajuste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+
+import { TiposService } from '../app/services/tipos.service';
+import { CategoriasService } from './services/categorias.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    TiposService,
+    CategoriasService,
+    HttpClientModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
