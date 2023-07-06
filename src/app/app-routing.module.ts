b7b3f53cc20f3e461a91b3db5a92b33deed313c1
filipedantas800 <filipetecnico800@hotmail.com>
2023-07-06 16:50:32 -0300

    Criando o primeiro formulario com reactive forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListagemCategoriasComponent } from './components/Categoria/listagem-categorias/listagem-categorias.component';
+import { NovaCategoriaComponent } from './components/Categoria/nova-categoria/nova-categoria.component';
 
 const routes: Routes = [
   {
     path: 'categorias/listagemcategorias', component: ListagemCategoriasComponent
+  },
+  {
+    path: 'categorias/novacategoria', component: NovaCategoriaComponent
   }
 ];
 
