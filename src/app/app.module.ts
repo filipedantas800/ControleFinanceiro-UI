18507e97ffb13bfe73878eb504d34dba9302447a
filipedantas800 <filipetecnico800@hotmail.com>
2023-07-06 16:19:57 -0300

    Tabela Categorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TiposService } from '../app/services/tipos.service';
@@ -9,7 +10,8 @@ import { CategoriasService } from './services/categorias.service';
 import { ListagemCategoriasComponent } from './components/Categoria/listagem-categorias/listagem-categorias.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
-import { AppRoutingModule } from './app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,10 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule
+    HttpClientModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [
     TiposService,
