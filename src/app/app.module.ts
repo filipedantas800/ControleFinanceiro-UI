a575470915239396d3bf8e0d95dbbe4e04f53348
filipedantas800 <filipetecnico800@hotmail.com>
2023-07-13 17:03:40 -0300

    Exclusão de categorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { TiposService } from '../app/services/tipos.service';
 import { CategoriasService } from './services/categorias.service';
 import { ListagemCategoriasComponent } from './components/Categoria/listagem-categorias/listagem-categorias.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NovaCategoriaComponent } from './components/Categoria/nova-categoria/nova-categoria.component';
+import { AtualizarCategoriaComponent } from './components/Categoria/atualizar-categoria/atualizar-categoria.component';
+import { DialogExclusaoCategoriasComponent } from './components/Categoria/listagem-categorias/listagem-categorias.component';
+
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { NovaCategoriaComponent } from './components/Categoria/nova-categoria/nova-categoria.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSelectModule } from '@angular/material/select';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { AtualizarCategoriaComponent } from './components/Categoria/atualizar-categoria/atualizar-categoria.component';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
   declarations: [
     AppComponent,
     ListagemCategoriasComponent,
     NovaCategoriaComponent,
-    AtualizarCategoriaComponent
+    AtualizarCategoriaComponent,
+    DialogExclusaoCategoriasComponent
   ],
   imports: [
     BrowserModule,
@@ -42,7 +45,8 @@ import { AtualizarCategoriaComponent } from './components/Categoria/atualizar-ca
     MatInputModule,
     MatDividerModule,
     MatSelectModule,
-    MatGridListModule
+    MatGridListModule,
+    MatDialogModule
   ],
   providers: [
     TiposService,
--- src/app/components/Categoria/listagem-categorias/listagem-categorias.component.ts
+++ src/app/components/Categoria/listagem-categorias/listagem-categorias.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { CategoriasService } from 'src/app/services/categorias.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-listagem-categorias',
@@ -12,7 +13,8 @@ export class ListagemCategoriasComponent implements OnInit {
   categorias = new MatTableDataSource<any>();
   displayedColumns!: string[];
 
-  constructor(private categoriasService: CategoriasService) { }
+  constructor(private categoriasService: CategoriasService,
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.categoriasService.PegarTodos().subscribe(resultado => {
@@ -24,5 +26,40 @@ export class ListagemCategoriasComponent implements OnInit {
 
   ExibirColunas(): string[] {
     return ['nome', 'icone', 'tipo', 'acoes']
+  }
+
+  AbrirDialog(categoriaId: any, nome: any): void {
+    this.dialog.open(DialogExclusaoCategoriasComponent, {
+      data: {
+        categoriaId: categoriaId,
+        nome: nome
+      },
+    })
+      .afterClosed()
+      .subscribe(resultado => {
+        if (resultado === true) {
+          debugger;
+          this.categoriasService.PegarTodos().subscribe(dados => {
+            this.categorias.data = dados;
+          });
+
+          this.displayedColumns = this.ExibirColunas();
+        }
+      });
+  }
+}
+
+@Component({
+  selector: 'app-dialog-exclusao-categorias',
+  templateUrl: 'dialog-exclusao-categorias.html'
+})
+export class DialogExclusaoCategoriasComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public dados: any,
+    private categoriasSevice: CategoriasService) { }
+
+  ExcluirCategoria(categoriaId: any): void {
+    this.categoriasSevice.ExcluirCategoria(categoriaId).subscribe(resultado => {
+
+    });
   }
 }
