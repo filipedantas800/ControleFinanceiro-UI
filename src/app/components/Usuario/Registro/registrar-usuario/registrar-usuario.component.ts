4d634a7cfca3a024e39976a2de9f11ba484289f7
filipedantas800 <filipetecnico800@hotmail.com>
2024-03-07 17:22:57 -0300

    ajuste node

--- src/app/components/Usuario/Registro/registrar-usuario/registrar-usuario.component.ts
+++ src/app/components/Usuario/Registro/registrar-usuario/registrar-usuario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { DadosRegistro } from 'src/app/models/DadosRegistro';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
@@ -12,7 +13,7 @@ export class RegistrarUsuarioComponent implements OnInit {
 
   formulario: any;
   foto: File = null as any;
-  erros: string[] | undefined;
+  erros: string[] = [];
 
   constructor(private usuariosService: UsuariosService,
     private router: Router) { }
@@ -34,4 +35,55 @@ export class RegistrarUsuarioComponent implements OnInit {
     return this.formulario.controls;
   }
 
+  SelecionarFoto(fileInput: any): void {
+    this.foto = fileInput.target.files[0] as File;
+    const reader = new FileReader();
+    reader.onload = function (e: any) {
+      // document.getElementById('foto').removeAttribute('hidden');
+      // document.getElementById('foto').setAttribute('src', e.target.result);
+    };
+
+    reader.readAsDataURL(this.foto);
+  }
+
+  EnviarFormulario(): void {
+    this.erros = [];
+    const usuario = this.formulario.value;
+    const formData: FormData = new FormData();
+
+    if (this.foto != null) {
+      formData.append('file', this.foto, this.foto.name);
+    }
+
+    this.usuariosService.SalvarFoto(formData).subscribe((resultado) => {
+      const dadosRegistro: DadosRegistro = new DadosRegistro();
+      dadosRegistro.nomeUsuario = usuario.nomeusuario;
+      dadosRegistro.cpf = usuario.cpf;
+      dadosRegistro.foto = resultado.foto;
+      dadosRegistro.profissao = usuario.profissao;
+      dadosRegistro.email = usuario.email;
+      dadosRegistro.senha = usuario.senha;
+
+      this.usuariosService.RegistrarUsuario(dadosRegistro).subscribe(
+        (dados) => {
+          const emailUsuarioLogado = dados.emailUsuarioLogado;
+          const usuarioId = dados.usuarioId;
+          const tokenUsuarioLogado = dados.tokenUsuarioLogado;
+          localStorage.setItem('EmailUsuarioLogado', emailUsuarioLogado);
+          localStorage.setItem('UsuarioId', usuarioId);
+          localStorage.setItem('TokenUsuarioLogado', tokenUsuarioLogado);
+          this.router.navigate(['/dashboard/index']);
+        },
+        (err) => {
+          if (err.status === 400) {
+            for (const campo in err.error.errors) {
+              if (err.error.errors.hasOwnProperty(campo)) {
+                this.erros.push(err.error.errors[campo]);
+              }
+            }
+          }
+        }
+      );
+    });
+  }
 }
